15a7d849007a41697b1df5d8584cecbdc400b99d
DonnyDunamis <mjd_mail@yahoo.com>
2022-02-28 21:25:31 -0800

    initial commit 2

--- stranger_Things/src/components/Register.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useState } from "react";
-
-const Register = () => {
- const [userName, setUsername] = useState('');
- const [password, setPassword] = useState('');
-}
-
-export default Register;
--- stranger_Things/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Posts } from "./Posts";
-export { default as Login } from "./Login";
-export { default as Register } from "./Register";
