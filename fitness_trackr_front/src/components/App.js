d31d05f73701afcc42418da9a86027a0fca09e17
Mark Angelo Dabu <markangelo.dabu@gmail.com>
2022-03-01 22:36:39 -0800

    set up completed

---
+++ fitness_trackr_front/src/components/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Home from './Home';
+import Routines from './Routines'
+import {Routes, Route} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/routines" element={<Routines />}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ fitness_trackr_front/src/components/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <h2>Welcome to Fitness Trackr</h2>
+    )
+}
+
+export default Home;
