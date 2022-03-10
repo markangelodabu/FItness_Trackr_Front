2882b917b83dddaa4edcfbad452e673d243ce945
DonnyDunamis <mjd_mail@yahoo.com>
2022-03-10 00:56:58 -0800

    fixed adding activity to routine

--- fitness_trackr_front/src/components/MyActivities.js
+++ fitness_trackr_front/src/components/MyActivities.js
@@ -3,6 +3,7 @@ import AddActivity from "./AddActivity";
 const MyActivities = ({ user, token, setActivities, activities}) => {
     return (
         <>
+        <h1>Activities</h1> 
             <AddActivity user={user} token={token} activities={activities} setActivities={setActivities} />
         </>
     );
